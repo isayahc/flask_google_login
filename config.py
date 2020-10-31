7d53ee340d2c67dedd047483916d0e6d53b9f634
Isayah Culbertson <isayahculbertson@gmail.com>
2020-10-31 14:34:44 -0400

    fixed configurations

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
-from os import environ
+"""Flask configuration variables."""
+# This is where you can change env
+from os import environ, path
+
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, ".env"))  # use for different .envs
 
 
-class Config(object):
-    DEBUG = False
-    TESTING = True
+class Config:
+    """Set Flask configuration from .env file."""
+
+    # General Config
+    SECRET_KEY = environ.get("SECRET_KEY")
+    FLASK_APP = environ.get("FLASK_APP")
+    FLASK_ENV = environ.get("FLASK_ENV")
+    TESTING = True  # enable testing
 
     # Database
     SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-class ProductionConfig(Config):
-    DATABASE_URI = 'mysql://user@localhost/foo'
-
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-
-
-class TestingConfig(Config):
-    TESTING = True
