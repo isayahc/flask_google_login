67159a93351efea1400bd4f4e783e6f69dadcd9c
Isayah Culbertson <isayahculbertson@gmail.com>
2020-11-01 16:29:20 -0500

    made changes

--- config.py
+++ config.py
@@ -15,10 +15,18 @@ class Config:
     SECRET_KEY = environ.get("SECRET_KEY")
     FLASK_APP = environ.get("FLASK_APP")
     FLASK_ENV = environ.get("FLASK_ENV")
-    TESTING = True  # enable testing
 
     # Database
-    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = environ.get("DEV_SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
+class TestConfig(Config):
+    """Set Flask configuration from .env file."""
+
+    # General Config
+    TESTING = True  # enable testing
+
+    # Database
+    SQLALCHEMY_DATABASE_URI = environ.get("TEST_SQLALCHEMY_DATABASE_URI")
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -10,7 +10,7 @@ db = SQLAlchemy()
 def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
+    app.config.from_object('config.TestConfig')
 
     db.init_app(app)
 
