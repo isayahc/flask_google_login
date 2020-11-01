c471903abe2556e511b686284896cf3ce9e5c29b
Isayah Culbertson <isayahculbertson@gmail.com>
2020-11-01 10:16:47 -0500

    made config for test

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from os import environ, path
 from dotenv import load_dotenv
 
 basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, ".env"))  # use for different .envs
+load_dotenv(path.join(basedir, ".test.env"))  # use for different .envs
 
 
 class Config:
@@ -21,3 +21,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+print(Config.SQLALCHEMY_DATABASE_URI)
