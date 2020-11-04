7416068afe1a13c3bd18d14ef999d0bae767128f
Isayah Culbertson <isayahculbertson@gmail.com>
2020-11-04 14:38:10 -0500

    reconfiguring env and db

--- config.py
+++ config.py
@@ -24,3 +24,21 @@ class Config:
     # Google API
     GOOGLE_CLIENT_ID = environ.get("GOOGLE_CLIENT_ID")
     GOOGLE_CLIENT_SECRET = environ.get("GOOGLE_CLIENT_SECRET")
+
+
+class ProductionConfig(Config):
+    """Set Flask configuration from .env file."""
+
+    # General Config
+    SECRET_KEY = environ.get("SECRET_KEY")
+    FLASK_APP = environ.get("FLASK_APP")
+    FLASK_ENV = environ.get("FLASK_ENV")
+
+    # Database
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Google API
+    GOOGLE_CLIENT_ID = environ.get("GOOGLE_CLIENT_ID")
+    GOOGLE_CLIENT_SECRET = environ.get("GOOGLE_CLIENT_SECRET")
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -15,7 +15,7 @@ db = SQLAlchemy()
 def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
+    app.config.from_object('config.ProductionConfig')
 
     from .model import db
 
