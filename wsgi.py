3accf6474a85296b9b782de29babd215db94b8cf
Isayah Culbertson <isayahculbertson@gmail.com>
2020-11-06 11:55:34 -0500

    this is a batch script cuz i'm repeating myself too much HAHAHAHAHAHHAH

--- wsgi.py
+++ wsgi.py
@@ -3,8 +3,6 @@ from flaskr import create_app
 # from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = create_app()
-# server = server
-# app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
